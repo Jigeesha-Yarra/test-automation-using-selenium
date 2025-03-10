869fd86aee96d7ad3367d3806172235c457ec770
Jigeesha <jigeeshayarra@gmail.com>
2025-03-10 16:28:46 +0530

    added comments

--- index.py
+++ index.py
@@ -3,19 +3,22 @@ from selenium.webdriver.common.by import By
 import time 
 
 driver = webdriver.Chrome()
+# driver is a brower-specific-program that allows selenium to communicate wth browser and execute test cases.
+# webdriver is a tool that automates web browser interactions and tests web applications.
 
 def test_login():
     driver.get("https://practicetestautomation.com/practice-test-login/")
     time.sleep(2)
-    username = driver.find_element(By.ID, "username")
+    username = driver.find_element(By.ID, "username") # 1st element with id attribute value matching the location i'll be returned
     password = driver.find_element(By.ID, "password")
     login_button = driver.find_element(By.ID, "submit")
     username.send_keys("student")
     password.send_keys("Password123")
+    # send_keys is to send keyboard input such as characters, numbers and symbols to text boxes inside an application. 
     login_button.click()
     time.sleep(2)
     assert "Logged In Successfully" in driver.page_source, "Login failed!"
-  
+    # assert is a statement that verifies if an application's behaviour is as expected.
 
 def test_form_validation():
     driver.get("https://www.w3schools.com/html/html_forms.asp")
